9967fc3fa3d1eac400c6b769d4d2cd957c1d7e6b
YJ <104505841+tosilan@users.noreply.github.com>
2025-02-10 20:41:58 +0900

    Update removesongfromplaylist.js

--- src/commands/removesongfromplaylist.js
+++ src/commands/removesongfromplaylist.js
@@ -1,32 +1,32 @@
 const { SlashCommandBuilder } = require('discord.js');
-const Playlist = require('../models/Playlist');
+const Playlist = require('../models/Playlist');  // プレイリストモデルをインポート
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('removesongfromplaylist')
-    .setDescription('Remove a song from a playlist.')
+    .setName('removesongfromplaylist')  // コマンド名
+    .setDescription('プレイリストから曲を削除します。')  // コマンドの説明
     .addStringOption(option => 
-      option.setName('name')
-        .setDescription('Name of the playlist.')
-        .setRequired(true))
+      option.setName('name')  // プレイリスト名のオプション
+        .setDescription('プレイリストの名前。')
+        .setRequired(true))  // 必須オプション
     .addStringOption(option => 
-      option.setName('song')
-        .setDescription('URL or name of the song.')
-        .setRequired(true)),
+      option.setName('song')  // 曲のオプション
+        .setDescription('曲のURLまたは名前。')
+        .setRequired(true)),  // 必須オプション
   
-  async execute(interaction) {
-    const name = interaction.options.getString('name');
-    const song = interaction.options.getString('song');
-    const userId = interaction.user.id;
+  async execute(interaction) {  // コマンド実行時の処理
+    const name = interaction.options.getString('name');  // プレイリスト名を取得
+    const song = interaction.options.getString('song');  // 曲を取得
+    const userId = interaction.user.id;  // ユーザーIDを取得
 
-    const playlist = await Playlist.findOne({ name, userId });
+    const playlist = await Playlist.findOne({ name, userId });  // プレイリストをデータベースから取得
     if (!playlist) {
-      return interaction.reply('Playlist not found or you do not have permission to modify it.');
+      return interaction.reply('プレイリストが見つからないか、変更する権限がありません。');  // エラーメッセージを返す
     }
 
-    playlist.songs = playlist.songs.filter(s => s !== song);
-    await playlist.save();
+    playlist.songs = playlist.songs.filter(s => s !== song);  // 曲を削除
+    await playlist.save();  // プレイリストを保存
 
-    await interaction.reply(`Song **${song}** has been removed from **${name}**.`);
+    await interaction.reply(`曲 **${song}** が **${name}** から削除されました。`);  // 成功メッセージを返す
   },
 };
